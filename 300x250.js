fa39cff15db2ccbfeb79c0f2ae24ffeefb173acb
herysoecahyono <herysoecahyono@users.noreply.github.com>
2024-02-07 00:03:35 +0700

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,9 @@
+	atOptions = {
+		'key' : 'd1df290a319f53d5b4be6cec7ad48dfe',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//compresssavvydetected.com/d1df290a319f53d5b4be6cec7ad48dfe/invoke.js"></scr' + 'ipt>');
+</script>
